5dc927efb417ee6efb8247285b2c4ca1874a47e9
Matheus Mello <matheusgmello1@gmail.com>
2024-03-26 18:24:40 -0300

    feat:  validação e formatação de erros

---
+++ src/main/java/br/com/matheusgmello/gestao_vagas/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,12 @@
+package br.com.matheusgmello.gestao_vagas.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+  
+  private String message;
+  private String field;
+}
--- src/main/java/br/com/matheusgmello/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/matheusgmello/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,6 +2,11 @@ package br.com.matheusgmello.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,8 +14,15 @@ public class CandidateEntity {
 
   private UUID id;
   private String name;
+
+  @NotBlank
+  @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
   private String username;
+
+  @Email(message = "O campo [email] deve conter um endereço válido")
   private String email;
+
+  @Length(min = 10, max = 100, message = "A senha deve conter no mínimo 10 caracteres")
   private String password;
   private String description;
   private String curriculum;
