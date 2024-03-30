0573d7627f46ec0f55ab9a3e41244177c456af3a
Matheus Mello <matheusgmello1@gmail.com>
2024-03-30 10:52:09 -0300

    feat: caso de uso criação de company

---
+++ src/main/java/br/com/matheusgmello/gestao_vagas/modules/company/repositories/CompanyRepository.java
@@ -0,0 +1,12 @@
+package br.com.matheusgmello.gestao_vagas.modules.company.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.matheusgmello.gestao_vagas.modules.company.entities.CompanyEntity;
+
+public interface CompanyRepository extends JpaRepository<CompanyEntity, UUID> {
+  Optional<CompanyEntity> findByUsernameOrEmail(String name, String email);
+}
