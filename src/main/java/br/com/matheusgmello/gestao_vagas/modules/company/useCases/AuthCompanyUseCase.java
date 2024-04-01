39093eb736253c218bce43bff431f1ac3b07ab00
Matheus Mello <matheusgmello1@gmail.com>
2024-04-01 17:28:17 -0300

    feat: recuperação e validação do token

--- src/main/java/br/com/matheusgmello/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/matheusgmello/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,5 +1,8 @@
 package br.com.matheusgmello.gestao_vagas.modules.company.useCases;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,10 +41,11 @@ public class AuthCompanyUseCase {
     }
 
     Algorithm algorithm = Algorithm.HMAC256(secretKey);
-    var token =JWT.create().withIssuer("javagas")
-      .withSubject(company.getId().toString())
-      .sign(algorithm);
-      return token;
 
+    var token = JWT.create().withIssuer("javagas")
+        .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
+        .withSubject(company.getId().toString()).sign(algorithm);
+
+    return token;
   }
 }
