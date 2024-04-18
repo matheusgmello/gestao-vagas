68f22787b7b99f3d17506ac05f360dd7bed61f63
Matheus Mello <matheusgmello1@gmail.com>
2024-04-18 10:57:31 -0300

    feat: token de candidato

--- src/main/java/br/com/matheusgmello/gestao_vagas/modules/candidate/CandidateRepository.java
+++ src/main/java/br/com/matheusgmello/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
   Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
+  Optional<CandidateEntity> findByUsername(String username);
 }
---
+++ src/main/java/br/com/matheusgmello/gestao_vagas/modules/candidate/dto/AuthCandidateRequestDTO.java
@@ -0,0 +1,5 @@
+package br.com.matheusgmello.gestao_vagas.modules.candidate.dto;
+
+public record AuthCandidateRequestDTO(String username, String password) {
+  
+}
