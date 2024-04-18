f5513e9cdc9f20a3d3edca35b0b5305dd84ad0de
Matheus Mello <matheusgmello1@gmail.com>
2024-04-18 18:42:55 -0300

    feat: rota de perfil do candidato

--- src/main/java/br/com/matheusgmello/gestao_vagas/modules/candidate/controllers/AuthCandidateController.java
+++ src/main/java/br/com/matheusgmello/gestao_vagas/modules/candidate/controllers/AuthCandidateController.java
@@ -2,7 +2,6 @@ package br.com.matheusgmello.gestao_vagas.modules.candidate.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
