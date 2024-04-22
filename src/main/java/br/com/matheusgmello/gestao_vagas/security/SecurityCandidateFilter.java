db460720815c9028ff86362568a7caee2a1b4396
Matheus Mello <matheusgmello1@gmail.com>
2024-04-22 12:22:50 -0300

    feat: configurando PreAuthorize

--- src/main/java/br/com/matheusgmello/gestao_vagas/security/SecurityCandidateFilter.java
+++ src/main/java/br/com/matheusgmello/gestao_vagas/security/SecurityCandidateFilter.java
@@ -1,8 +1,11 @@
 package br.com.matheusgmello.gestao_vagas.security;
 
 import java.io.IOException;
+import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -22,7 +25,7 @@ public class SecurityCandidateFilter extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    SecurityContextHolder.getContext().setAuthentication(null);
+    // SecurityContextHolder.getContext().setAuthentication(null);
     String header = request.getHeader("Authorization");
 
     if (request.getRequestURI().startsWith("/candidate")) {
@@ -35,7 +38,15 @@ public class SecurityCandidateFilter extends OncePerRequestFilter {
         }
 
         request.setAttribute("candidate_id", token.getSubject());
-        var roles = token.getClaim("roles");
+        var roles = token.getClaim("roles").asList(Object.class);
+
+        var grants = roles.stream()
+            .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase()))
+            .toList();
+
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(token.getSubject(), null,
+            grants);
+        SecurityContextHolder.getContext().setAuthentication(auth);
       }
 
     }
