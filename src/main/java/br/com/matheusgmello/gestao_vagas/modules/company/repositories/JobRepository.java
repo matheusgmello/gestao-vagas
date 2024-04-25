dc9c4a6425926515314a5e9120b3adcd29aa67ca
Matheus Mello <matheusgmello1@gmail.com>
2024-04-25 11:18:14 -0300

    feat: caso de uso para listagem de trabalhos com filtro

--- src/main/java/br/com/matheusgmello/gestao_vagas/modules/company/repositories/JobRepository.java
+++ src/main/java/br/com/matheusgmello/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -1,5 +1,6 @@
 package br.com.matheusgmello.gestao_vagas.modules.company.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.matheusgmello.gestao_vagas.modules.company.entities.JobEntity;
 
 public interface JobRepository extends JpaRepository<JobEntity, UUID> {
+
+
+  List<JobEntity> findByDescriptionContaining(String filter);
 }
