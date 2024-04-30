a5ef52bd1fe402cfe9d1e0d4e7f6c879f82357d9
Matheus Mello <matheusgmello1@gmail.com>
2024-04-30 10:22:23 -0300

    test: sucesso do apply job

--- src/main/java/br/com/matheusgmello/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
+++ src/main/java/br/com/matheusgmello/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import br.com.matheusgmello.gestao_vagas.exceptions.JobNotFoundException;
 import br.com.matheusgmello.gestao_vagas.exceptions.UserNotFoundException;
 import br.com.matheusgmello.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.matheusgmello.gestao_vagas.modules.candidate.entity.ApplyJobEntity;
 import br.com.matheusgmello.gestao_vagas.modules.candidate.repository.ApplyJobRepository;
 import br.com.matheusgmello.gestao_vagas.modules.company.repositories.JobRepository;
 
@@ -24,7 +25,7 @@ public class ApplyJobCandidateUseCase {
     private ApplyJobRepository applyJobRepository;
 
 
-    public void execute(UUID idCandidate, UUID idJob){
+    public ApplyJobEntity execute(UUID idCandidate, UUID idJob){
  
         this.candidateRepository.findById(idCandidate)
         .orElseThrow(() -> {
@@ -37,6 +38,12 @@ public class ApplyJobCandidateUseCase {
             throw new JobNotFoundException();
         });
 
+        var applyJob = ApplyJobEntity.builder()
+        .candidateId(idCandidate)
+        .jobId(idJob).build();
+
+        applyJob = applyJobRepository.save(applyJob);
+        return applyJob;
 
     }
 }
